dc6b1d22acdfa777ea66ef1729d9877394074679
Vadim Titov <titov.hse@gmail.com>
2025-05-17 13:21:28 +0100

    add s3 dataset class

--- src/datarush/utils/s3_client.py
+++ src/datarush/utils/s3_client.py
@@ -1,11 +1,20 @@
 """S3 client wrapper for basic file and folder operations."""
 
+from enum import StrEnum
 from io import BytesIO
+from typing import Any, Sequence
 
+import awswrangler as wr
 import boto3
+import pandas as pd
 from botocore.client import Config
 
 from datarush.config import S3Config
+from datarush.core.types import ContentType
+
+
+class DatasetDoesNotExistError(Exception):
+    """Exception raised when a dataset does not exist."""
 
 
 class S3Client:
@@ -49,3 +58,125 @@ class S3Client:
         if prefix == "/":
             return list({key.split("/")[0] for key in keys})
         return list({key.split(prefix)[1].split("/")[0] for key in keys})
+
+
+class DatasetWriteMode(StrEnum):
+    """Write mode for S3 dataset sink."""
+
+    OVERWRITE = "overwrite"
+    OVERWRITE_PARTITIONS = "overwrite_partitions"
+    APPEND = "append"
+
+
+class S3Dataset:
+    """S3 Dataset Client."""
+
+    def __init__(
+        self,
+        bucket: str,
+        prefix: str,
+        content_type: ContentType,
+        partition_columns: Sequence[str] | None = None,
+        unique_ids: Sequence[str] | None = None,
+        write_mode: DatasetWriteMode = DatasetWriteMode.APPEND,
+        config: S3Config | None = None,
+    ) -> None:
+        """Initialize the S3 dataset client with configuration."""
+        self._content_type = content_type
+        self._path = f"s3://{bucket}/{prefix.strip('/')}"
+
+        self._write_mode = write_mode.value
+        self._partition_columns = partition_columns or []
+        self._unique_ids = unique_ids or []
+
+        self._config = config or S3Config.fromenv()
+        self._session = boto3.Session(
+            aws_access_key_id=self._config.access_key,
+            aws_secret_access_key=self._config.secret_key.reveal(),
+        )
+        wr.config.s3_endpoint_url = self._config.endpoint
+
+    def read(self, **kwargs: Any) -> pd.DataFrame:
+        """Read a dataset from S3."""
+        common_kwargs = dict(
+            boto3_session=self._session,
+            path=self._path,
+            dataset=True,
+        )
+
+        try:
+            if self._content_type == ContentType.JSON:
+                df = wr.s3.read_json(**common_kwargs, **kwargs)
+            elif self._content_type == ContentType.CSV:
+                df = wr.s3.read_csv(**common_kwargs, **kwargs)
+            elif self._content_type == ContentType.PARQUET:
+                df = wr.s3.read_parquet(**common_kwargs, **kwargs)
+            else:
+                raise ValueError(f"Unsupported content type: {self._content_type}")
+        except wr.exceptions.NoFilesFound:
+            raise DatasetDoesNotExistError(f"Dataset does not exist at {self._path}")
+
+        return df
+
+    def write(self, df: pd.DataFrame, **kwargs: Any) -> None:
+        """Write a DataFrame to S3."""
+        if df.empty:
+            return
+
+        if self._unique_ids and self._write_mode != DatasetWriteMode.APPEND:
+            raise ValueError("unique_ids are only supported in APPEND mode. ")
+
+        if self._unique_ids:
+            self._write_unique(df, **kwargs)
+        else:
+            self._write(df, **kwargs)
+
+    def _write(self, df: pd.DataFrame, **kwargs: Any) -> None:
+        if df.empty:
+            return
+
+        common_kwargs = dict(
+            df=df,
+            path=self._path,
+            dataset=True,
+            boto3_session=self._session,
+            partition_cols=self._partition_columns,
+            mode=self._write_mode,
+            index=False,
+        )
+
+        if self._content_type == ContentType.JSON:
+            wr.s3.to_json(orient="records", lines=True, **common_kwargs)
+        elif self._content_type == ContentType.CSV:
+            wr.s3.to_csv(**common_kwargs)
+        elif self._content_type == ContentType.PARQUET:
+            wr.s3.to_parquet(**common_kwargs)
+        else:
+            raise ValueError(f"Unsupported content type: {self._content_type}")
+
+    def _write_unique(self, df: pd.DataFrame, **kwargs: Any) -> None:
+        """Write a DataFrame to S3 with unique IDs."""
+        unique_ids = list(self._partition_columns) + list(self._unique_ids)
+
+        missing_columns = [col for col in unique_ids if col not in df.columns]
+        if missing_columns:
+            raise ValueError(f"Columns {missing_columns} not found in DataFrame columns.")
+
+        if self._partition_columns:
+            unique_partitions = df[self._partition_columns].drop_duplicates().to_dict("records")
+            unique_partitions_set = set(frozenset(d.items()) for d in unique_partitions)
+
+            def partition_filter(row: dict[str, str]) -> bool:
+                return frozenset(row.items()) in unique_partitions_set
+
+        else:
+            partition_filter = None  # type: ignore
+
+        try:
+            current_df = self.read(partition_filter=partition_filter)
+            df = df.merge(current_df[unique_ids], on=unique_ids, how="left", indicator=True)
+            df = df[df["_merge"] == "left_only"].drop(columns=["_merge"])
+        except DatasetDoesNotExistError:
+            pass
+
+        self._write(df, **kwargs)
--- tests/unit/test_s3_client.py
+++ tests/unit/test_s3_client.py
@@ -1,9 +1,16 @@
 from io import BytesIO
 from unittest.mock import MagicMock, patch
 
+import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
 
-from datarush.utils.s3_client import S3Client
+from datarush.core.types import ContentType
+from datarush.utils.s3_client import (
+    DatasetWriteMode,
+    S3Client,
+    S3Dataset,
+)
 
 
 @pytest.fixture
@@ -133,3 +140,124 @@ def test_list_folders_with_prefix(s3_client, mock_boto3_client):
     mock_client_instance.list_objects_v2.assert_called_once_with(
         Bucket="test-bucket", Prefix="prefix"
     )
+
+
+# DATASET
+
+
+@pytest.fixture
+def mock_awswrangler():
+    with patch("datarush.utils.s3_client.wr") as mock_wr:
+        yield mock_wr
+
+
+@pytest.fixture
+def mock_boto3_session():
+    with patch("datarush.utils.s3_client.boto3.Session") as mock_session:
+        mock_session_instance = MagicMock()
+        mock_session.return_value = mock_session_instance
+        yield mock_session_instance
+
+
+@pytest.fixture
+def s3_dataset(mock_awswrangler, mock_boto3_session):
+    return S3Dataset(
+        bucket="test-bucket",
+        prefix="test-prefix",
+        content_type=ContentType.CSV,
+        partition_columns=["col1"],
+        unique_ids=["id"],
+        write_mode=DatasetWriteMode.APPEND,
+    )
+
+
+def test_read_csv(mock_awswrangler, mock_boto3_session, s3_dataset):
+    # Mock the read_csv method
+    mock_awswrangler.s3.read_csv.return_value = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+
+    # Call the method
+    result = s3_dataset.read()
+
+    # Assertions
+    mock_awswrangler.s3.read_csv.assert_called_once_with(
+        boto3_session=mock_boto3_session,
+        path="s3://test-bucket/test-prefix",
+        dataset=True,
+    )
+    assert isinstance(result, pd.DataFrame)
+    assert result.equals(pd.DataFrame({"col1": [1, 2], "col2": [3, 4]}))
+
+
+def test_write_csv(mock_awswrangler, mock_boto3_session, s3_dataset):
+    # Mock the DataFrame
+    df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4], "id": [10, 20]})
+    # Mock the read method to return an existing DataFrame
+    mock_awswrangler.s3.read_csv.return_value = pd.DataFrame(
+        {"col1": [8], "id": [888], "col2": [8]}
+    )
+
+    # Call the method
+    s3_dataset.write(df)
+
+    # Assertions
+    mock_awswrangler.s3.to_csv.assert_called_once()
+    _, called_kwargs = mock_awswrangler.s3.to_csv.call_args
+
+    assert_frame_equal(called_kwargs.pop("df"), df)
+    assert called_kwargs == dict(
+        path="s3://test-bucket/test-prefix",
+        dataset=True,
+        boto3_session=mock_boto3_session,
+        partition_cols=["col1"],
+        mode=DatasetWriteMode.APPEND.value,
+        index=False,
+    )
+
+
+def test_write_empty_dataframe(mock_awswrangler, s3_dataset):
+    # Mock an empty DataFrame
+    df = pd.DataFrame()
+
+    # Call the method
+    s3_dataset.write(df)
+
+    # Assertions
+    mock_awswrangler.s3.to_csv.assert_not_called()  # No write should occur
+
+
+def test_write_unique(mock_awswrangler, mock_boto3_session, s3_dataset):
+    # Mock the DataFrame
+    df = pd.DataFrame({"col1": [1, 2], "id": [10, 20], "col2": [3, 4]})
+    to_append_expected = pd.DataFrame({"col1": [2], "id": [20], "col2": [4]})
+
+    # Mock the read method to return an existing DataFrame
+    mock_awswrangler.s3.read_csv.return_value = pd.DataFrame(
+        {"col1": [1], "id": [10], "col2": [3]}
+    )
+
+    # Call the method
+    s3_dataset.write(df)
+
+    # Assertions
+    mock_awswrangler.s3.to_csv.assert_called_once()
+    _, called_kwargs = mock_awswrangler.s3.to_csv.call_args
+
+    to_append_actual = called_kwargs.pop("df").reset_index(drop=True)
+    assert_frame_equal(to_append_actual, to_append_expected)
+    assert called_kwargs == dict(
+        path="s3://test-bucket/test-prefix",
+        dataset=True,
+        boto3_session=mock_boto3_session,
+        partition_cols=["col1"],
+        mode=DatasetWriteMode.APPEND.value,
+        index=False,
+    )
+
+
+def test_write_unique_missing_columns(mock_awswrangler, s3_dataset):
+    # Mock the DataFrame with missing unique_ids
+    df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+
+    # Call the method and expect an exception
+    with pytest.raises(ValueError, match="Columns \\['id'\\] not found in DataFrame columns."):
+        s3_dataset.write(df)
