cecd45a5b32958cf2077be13f852deed39f3e3d6
Vadim Titov <titov.hse@gmail.com>
2025-05-26 17:36:53 +0100

    update filter_row to use new condition

--- src/datarush/core/operations/transformations/filter_row.py
+++ src/datarush/core/operations/transformations/filter_row.py
@@ -3,33 +3,38 @@
 from pydantic import Field
 
 from datarush.core.dataflow import Operation, Tableset
-from datarush.core.types import BaseOperationModel, ColumnStr, TableStr
+from datarush.core.types import BaseOperationModel, TableStr
+from datarush.utils.conditions import RowCondition, match_conditions
 
 
 class FilterRowModel(BaseOperationModel):
     """Filter row operation model."""
 
     table: TableStr = Field(title="Table", description="Table to filter")
-    column: ColumnStr = Field(title="Column", description="Column to filter by")
-    value: str = Field(title="Value", description="Value to filter for")
+    condition: RowCondition = Field(
+        title="Condition",
+        description="Condition to filter rows by",
+    )
 
 
 class FilterByColumn(Operation):
     """Filter row by column value operation."""
 
-    name = "filter"
-    title = "Filter"
+    name = "filter_rows"
+    title = "Filter Rows"
     description = "Filter table rows by column value"
     model: FilterRowModel
 
     def summary(self) -> str:
         """Provide operation summary."""
-        return f"Filter `{self.model.table}` where {self.model.column} is {self.model.value}"
+        return f"Filter `{self.model.table}` where {self.model.condition.summary()}"
 
     def operate(self, tableset: Tableset) -> Tableset:
         """Run operation."""
-        table, column, value = self.model.table, self.model.column, self.model.value
+        table = self.model.table
         df = tableset.get_df(table)
-        filtered_df = df[df[column] == value]
-        tableset.set_df(table, filtered_df)
+
+        mask = match_conditions(df, [self.model.condition])
+
+        tableset.set_df(table, df[mask])
         return tableset
